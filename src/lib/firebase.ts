65ff7be84907852a90147157fb7919e2000846d5
Viraj Srivastav <virajsrivastav1112@gmail.com>
2025-08-28 03:16:33 +0000

    env file me firebase add to kr tb hi to hoga login id pss

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,21 @@
+
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "leadtrack-pulse",
-  "appId": "1:972472117593:web:d3aca4dfbc51f8116f0448",
-  "storageBucket": "leadtrack-pulse.firebasestorage.app",
-  "apiKey": "AIzaSyD96cJIAe4rtL6b5pxU35kSzKL7Lkl-Q0w",
-  "authDomain": "leadtrack-pulse.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "972472117593"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
